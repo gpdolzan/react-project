ac660d271feb527e58d6c6f9a8f1d8f9a2394636
Gabriel Dolzan <gabrieldolzan@icloud.com>
2022-03-12 17:49:40 -0300

    Added a Scrollable-Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { Component } from 'react';
 import SearchBox from './SearchBox'
 import CardList from './CardList';
-import { robots } from './Robots';
+import Scroll from './Scroll'
 import 'tachyons'
 
 class App extends Component
@@ -12,13 +12,19 @@ class App extends Component
   {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     }
   }
 
   onSearchChange = (event) => {
     this.setState({searchField: event.target.value })
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   render()
@@ -32,13 +38,26 @@ class App extends Component
       );
     })
 
-    return(
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    )
+    if(this.state.robots.length === 0)
+    {
+      return <h1>Loading</h1>
+    }
+    else
+    {
+      return(
+        <div>
+          <div className='tc'>
+            <Scroll >
+              <h1 className='f1'>RoboFriends</h1>
+              <SearchBox searchChange={this.onSearchChange} />
+            </Scroll>
+          </div>
+          <div className='tc pt7'>
+            <CardList robots={filteredRobots} />
+          </div>
+        </div>
+      )
+    }
   }
 }
 export default App;
---
+++ src/Scroll.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import './Scroll.css'
+
+const Scroll = (props) =>{
+    return(
+        <div className="scroll">
+            {props.children}
+        </div>
+    )
+};
+
+export default Scroll
