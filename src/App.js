859e9eb5da9aaa50f227295f7a3c5984c4d81565
Gabriel Dolzan <gabrieldolzan@icloud.com>
2022-03-12 05:19:54 -0300

    Added a custom font and a cool background

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SearchBox from './SearchBox'
 import CardList from './CardList';
 import { robots } from './Robots';
+import 'tachyons'
 
 class App extends Component
 {
@@ -33,7 +34,7 @@ class App extends Component
 
     return(
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
